b06cb32f85034db44f89882fcaacc805242d90b6
Robey Pointer <robey@twitter.com>
2008-08-17 22:24:36 -0700

    namespace the MemcacheResponse case classes, and fix encoder to return a type that mina understands

--- src/main/scala/com/twitter/tomservo/MemcacheClientDecoder.scala
+++ src/main/scala/com/twitter/tomservo/MemcacheClientDecoder.scala
@@ -1,45 +1,48 @@
 package com.twitter.tomservo
 
+import org.apache.mina.core.buffer.IoBuffer
 import org.apache.mina.core.session.IoSession
 import org.apache.mina.filter.codec.{ProtocolCodecFilter, ProtocolEncoder, ProtocolEncoderOutput}
 import com.twitter.tomservo.Steps._
 
 
-abstract sealed class ServerResponse
+abstract sealed class MemcachedResponse
 
-// 3 types of error that can be returned by the server
-case object Error extends ServerResponse
-case class ClientError(reason: String) extends ServerResponse
-case class ServerError(reason: String) extends ServerResponse
+object MemcachedResponse {
+  // 3 types of error that can be returned by the server
+  case object Error extends MemcachedResponse
+  case class ClientError(reason: String) extends MemcachedResponse
+  case class ServerError(reason: String) extends MemcachedResponse
 
-// when fetching stats or values:
-case class Value(key: String, flags: Int, casKey: String, data: Array[Byte]) extends ServerResponse {
-  // FIXME: why doesn't scala do array equals correctly?
-  override def equals(obj: Any) = {
-    obj match {
-      case Value(okey, oflags, ocasKey, odata) =>
-        (key == okey) && (flags == oflags) && (casKey == ocasKey) &&
-          (java.util.Arrays.equals(data, odata))
-      case _ => false
+  // when fetching stats or values:
+  case class Value(key: String, flags: Int, casKey: String, data: Array[Byte]) extends MemcachedResponse {
+    // FIXME: why doesn't scala do array equals correctly?
+    override def equals(obj: Any) = {
+      obj match {
+        case Value(okey, oflags, ocasKey, odata) =>
+          (key == okey) && (flags == oflags) && (casKey == ocasKey) &&
+            (java.util.Arrays.equals(data, odata))
+        case _ => false
+      }
     }
   }
+  case class StatItem(key: String, value: String) extends MemcachedResponse
+  case object EndOfResults extends MemcachedResponse
+
+  // set/add/replace/append/prepend/cas/delete/incr/decr:
+  case object NotFound extends MemcachedResponse
+
+  // set/add/replace/append/prepend/cas:
+  case object Stored extends MemcachedResponse
+  case object NotStored extends MemcachedResponse
+  case object Exists extends MemcachedResponse
+
+  // delete:
+  case object Deleted extends MemcachedResponse
+
+  // incr/decr:
+  case class NewValue(data: Long) extends MemcachedResponse
 }
-case class StatItem(key: String, value: String) extends ServerResponse
-case object EndOfResults extends ServerResponse
-
-// set/add/replace/append/prepend/cas/delete/incr/decr:
-case object NotFound extends ServerResponse
-
-// set/add/replace/append/prepend/cas:
-case object Stored extends ServerResponse
-case object NotStored extends ServerResponse
-case object Exists extends ServerResponse
-
-// delete:
-case object Deleted extends ServerResponse
-
-// incr/decr:
-case class NewValue(data: Long) extends ServerResponse
 
 
 object MemcacheClientDecoder {
@@ -47,39 +50,39 @@ object MemcacheClientDecoder {
     val parts = line.split(" ", 2)
     parts(0) match {
       case "ERROR" =>
-        state.out.write(Error)
+        state.out.write(MemcachedResponse.Error)
         End
 
       case "CLIENT_ERROR" =>
-        state.out.write(ClientError(if (parts.length == 2) parts(1) else "(unknown)"))
+        state.out.write(MemcachedResponse.ClientError(if (parts.length == 2) parts(1) else "(unknown)"))
         End
 
       case "SERVER_ERROR" =>
-        state.out.write(ServerError(if (parts.length == 2) parts(1) else "(unknown)"))
+        state.out.write(MemcachedResponse.ServerError(if (parts.length == 2) parts(1) else "(unknown)"))
         End
 
       case "NOT_FOUND" =>
-        state.out.write(NotFound)
+        state.out.write(MemcachedResponse.NotFound)
         End
 
       case "STORED" =>
-        state.out.write(Stored)
+        state.out.write(MemcachedResponse.Stored)
         End
 
       case "NOT_STORED" =>
-        state.out.write(NotStored)
+        state.out.write(MemcachedResponse.NotStored)
         End
 
       case "EXISTS" =>
-        state.out.write(Exists)
+        state.out.write(MemcachedResponse.Exists)
         End
 
       case "DELETED" =>
-        state.out.write(Deleted)
+        state.out.write(MemcachedResponse.Deleted)
         End
 
       case "END" =>
-        state.out.write(EndOfResults)
+        state.out.write(MemcachedResponse.EndOfResults)
         End
 
       case "VALUE" =>
@@ -93,7 +96,7 @@ object MemcacheClientDecoder {
           val bytes = subParts(2).toInt
           val casKey = if (subParts.length == 4) subParts(3) else ""
           readByteBuffer(bytes) { bytes =>
-            state.out.write(Value(key, flags, casKey, bytes))
+            state.out.write(MemcachedResponse.Value(key, flags, casKey, bytes))
             readByteBuffer(2) { lf =>
               if (new String(lf, "UTF-8") != "\r\n") {
                 throw new ProtocolError("Corrupted VALUE terminator")
@@ -109,7 +112,7 @@ object MemcacheClientDecoder {
       // incr/decr response:
       case value =>
         try {
-          state.out.write(NewValue(value.toLong))
+          state.out.write(MemcachedResponse.NewValue(value.toLong))
           End
         } catch {
           case e: NumberFormatException =>
@@ -124,9 +127,9 @@ object MemcacheClientDecoder {
     def encode(session: IoSession, message: AnyRef, out: ProtocolEncoderOutput) = {
       val data = message match {
         case s: String => s.getBytes
-        case b: Array[String] => b
+        case b: Array[Byte] => b
       }
-      out.write(data)
+      out.write(IoBuffer.wrap(data))
     }
 
     def dispose(session: IoSession): Unit = {
--- src/test/scala/com/twitter/tomservo/MemcacheClientDecoderSpec.scala
+++ src/test/scala/com/twitter/tomservo/MemcacheClientDecoderSpec.scala
@@ -12,7 +12,7 @@ object MemcacheClientDecoderSpec extends Specification {
 
   private var fakeSession: IoSession = null
   private var fakeDecoderOutput: ProtocolDecoderOutput = null
-  private var written: List[ServerResponse] = Nil
+  private var written: List[MemcachedResponse] = Nil
 
   def quickDecode(decoder: Decoder, s: String): Unit = quickDecode(decoder, s.getBytes)
   def quickDecode(decoder: Decoder, b: Array[Byte]): Unit = quickDecode(decoder, IoBuffer.wrap(b))
@@ -28,7 +28,7 @@ object MemcacheClientDecoderSpec extends Specification {
       fakeDecoderOutput = new ProtocolDecoderOutput {
         override def flush = {}
         override def write(obj: AnyRef) = {
-          written = written + obj.asInstanceOf[ServerResponse]
+          written = written + obj.asInstanceOf[MemcachedResponse]
         }
       }
     }
@@ -37,31 +37,35 @@ object MemcacheClientDecoderSpec extends Specification {
     "decode simple errors" in {
       val decoder = new Decoder(MemcacheClientDecoder.response)
       quickDecode(decoder, "CLIENT_ERROR you suck!\r\n")
-      written mustEqual List(ClientError("you suck!"))
+      written mustEqual List(MemcachedResponse.ClientError("you suck!"))
 
+      written = Nil
       quickDecode(decoder, "SERVER_ERROR i suck!\n")
-      written mustEqual List(ClientError("you suck!"), ServerError("i suck!"))
+      written mustEqual List(MemcachedResponse.ServerError("i suck!"))
 
+      written = Nil
       quickDecode(decoder, "ERROR\r\n")
-      written mustEqual List(ClientError("you suck!"), ServerError("i suck!"), Error)
+      written mustEqual List(MemcachedResponse.Error)
 
+      written = Nil
       quickDecode(decoder, "NOT_FOUND\r\n")
-      written mustEqual List(ClientError("you suck!"), ServerError("i suck!"), Error, NotFound)
+      written mustEqual List(MemcachedResponse.NotFound)
     }
 
     "decode values" in {
       val decoder = new Decoder(MemcacheClientDecoder.response)
       quickDecode(decoder, "VALUE hippos 124 5\r\nhello\r\n")
-      written mustEqual List(Value("hippos", 124, "", "hello".getBytes))
+      written mustEqual List(MemcachedResponse.Value("hippos", 124, "", "hello".getBytes))
 
+      written = Nil
       quickDecode(decoder, "END\r\n")
-      written mustEqual List(Value("hippos", 124, "", "hello".getBytes), EndOfResults)
+      written mustEqual List(MemcachedResponse.EndOfResults)
     }
 
     "decode incr/decr result" in {
       val decoder = new Decoder(MemcacheClientDecoder.response)
       quickDecode(decoder, "413\r\n")
-      written mustEqual List(NewValue(413))
+      written mustEqual List(MemcachedResponse.NewValue(413))
     }
 
     "decode values in chunks" in {
@@ -69,24 +73,27 @@ object MemcacheClientDecoderSpec extends Specification {
       quickDecode(decoder, "VALUE hippos 12")
       written mustEqual Nil
       quickDecode(decoder, "4 5\r\nhello\r\nVALUE hat 3 6 105\r\ncommie\r\nEND\r\n")
-      written mustEqual List(Value("hippos", 124, "", "hello".getBytes),
-                             Value("hat", 3, "105", "commie".getBytes),
-                             EndOfResults)
+      written mustEqual List(MemcachedResponse.Value("hippos", 124, "", "hello".getBytes),
+                             MemcachedResponse.Value("hat", 3, "105", "commie".getBytes),
+                             MemcachedResponse.EndOfResults)
     }
 
     "decode various simple responses" in {
       val decoder = new Decoder(MemcacheClientDecoder.response)
       quickDecode(decoder, "STORED\r\n")
-      written mustEqual List(Stored)
+      written mustEqual List(MemcachedResponse.Stored)
 
+      written = Nil
       quickDecode(decoder, "NOT_STORED\r\n")
-      written mustEqual List(Stored, NotStored)
+      written mustEqual List(MemcachedResponse.NotStored)
 
+      written = Nil
       quickDecode(decoder, "EXISTS\r\n")
-      written mustEqual List(Stored, NotStored, Exists)
+      written mustEqual List(MemcachedResponse.Exists)
 
+      written = Nil
       quickDecode(decoder, "DELETED\r\n")
-      written mustEqual List(Stored, NotStored, Exists, Deleted)
+      written mustEqual List(MemcachedResponse.Deleted)
     }
   }
 }
